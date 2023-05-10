b55ba1e5ecad73d64cc1aeb367ce761c7e842e03
kimdohwan <kimdohwan061019@gmail.com>
2023-05-10 10:22:13 +0900

    Initial Commit

---
+++ gitttttttttt/gittttttttttApp.swift
@@ -0,0 +1,17 @@
+//
+//  gittttttttttApp.swift
+//  gitttttttttt
+//
+//  Created by 김도환 on 2023/05/10.
+//
+
+import SwiftUI
+
+@main
+struct gittttttttttApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
