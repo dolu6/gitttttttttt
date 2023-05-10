2123319cdf4a4e5328a21ba1e8cb915576cc0088
kimdohwan <kimdohwan061019@gmail.com>
2023-05-10 11:55:06 +0900

    hi

---
+++ gitttttttttt/CapybaraCute.swift
@@ -0,0 +1,20 @@
+//
+//  CapybaraCute.swift
+//  gitttttttttt
+//
+//  Created by 김도환 on 2023/05/10.
+//
+
+import SwiftUI
+
+struct CapybaraCute: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CapybaraCute_Previews: PreviewProvider {
+    static var previews: some View {
+        CapybaraCute()
+    }
+}
