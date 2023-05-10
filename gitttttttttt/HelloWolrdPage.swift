9dcfa760b6c2f10d3e7b78ae7570d315048a10d0
Caleb <jjy050708@gmail.com>
2023-05-10 11:53:31 +0900

    Hello_World_Page

---
+++ gitttttttttt/HelloWolrdPage.swift
@@ -0,0 +1,20 @@
+//
+//  HelloWolrdPage.swift
+//  gitttttttttt
+//
+//  Created by 정주영 on 2023/05/10.
+//
+
+import SwiftUI
+
+struct HelloWolrdPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HelloWolrdPage_Previews: PreviewProvider {
+    static var previews: some View {
+        HelloWolrdPage()
+    }
+}
