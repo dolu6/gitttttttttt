139e8fb80a45c8a139ddadc53398e101c9f3468c
kimdohwan <kimdohwan061019@gmail.com>
2023-05-10 12:08:45 +0900

    Hi

---
+++ gitttttttttt/HiHi.swift
@@ -0,0 +1,20 @@
+//
+//  HiHi.swift
+//  gitttttttttt
+//
+//  Created by 김도환 on 2023/05/10.
+//
+
+import SwiftUI
+
+struct HiHi: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HiHi_Previews: PreviewProvider {
+    static var previews: some View {
+        HiHi()
+    }
+}
